1dd5be98d36ed3161cd9b8ac25c47e1e4b73019f
Erin <etruman2@gmail.com>
2021-12-08 12:03:44 -0600

    commit before lunch

--- src/components/communityResources/CommunityResourceList.js
+++ src/components/communityResources/CommunityResourceList.js
@@ -51,9 +51,6 @@ export const CommunityResourceList = (props) => {
                                     </CardContent>
                                 </Card>
 
-
-
-
                             </div>
                         </>
                     }
