a804e05853f1ec1d5cff06c48d2cd02913f0cde6
Erin <etruman2@gmail.com>
2021-12-17 15:07:59 -0600

    progress on the profile

--- src/components/reviewers/Profile.js
+++ src/components/reviewers/Profile.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { useHistory, Link, useParams } from "react-router-dom"
-import { getMyProfile, deleteProfile, updateProfile, getReviewerById  } from "./ReviewerManager";
+import { getMyProfile, deleteProfile, updateProfile, getReviewerById } from "./ReviewerManager";
 
 export const Profile = (props) => {
     console.log(props)
@@ -31,40 +31,35 @@ export const Profile = (props) => {
         deleteProfile(id, func)
     }
 
-    // const toggleForm = () => {
-    //     if (showComments == true) {
-    //         setShowComments(false)
-    //     } else {
-    //         setShowComments(true)
-    //     }
-    // }
-
     return (
         <>
 
-            <h2 className="title">My Reviews</h2>
+            <h2 className="title">{reviewer.user?.first_name} {reviewer?.user?.last_name}</h2>
+            <p>Username: {reviewer.user?.username} </p>
             <div className="allReviews">
 
-                
-                   
-                        return <>
-                            <div className="space-between">
-                            
-                                <p>Author: {reviewer.user?.first_name} {reviewer?.user?.last_name}</p>
-                                {/* <p>Date: {review.publication_date}</p> */}
-                                {/* <p>{review.content}</p> */}
-                                
-                                <div className="buttons">
-                                    {/* <button value={entry.id} onClick={() => { editEntry(entry.id) }}>EDIT</button> */}
-                                    <button className="btn" value={reviewer.id} onClick={() => { handleDelete(reviewer.id, fetchReviewer) }}>DELETE</button>
 
-                                </div>
-                            </div>
-                        </>
-                    
-                
 
-                
+
+                <div className="space-between">
+                <p></p>
+                <img src={reviewer.profile_pic} alt="profile picture" />
+                <p>Username: {reviewer.user?.username} </p>
+                <p>Email: {reviewer.user?.email} </p>
+                    {/* <p>Date: {review.publication_date}</p> */}
+                    {/* <p>{review.content}</p> */}
+
+                    <div className="buttons">
+                        {/* <button value={entry.id} onClick={() => { editEntry(entry.id) }}>EDIT</button> */}
+                        <button className="btn" value={reviewer.id} onClick={() => { handleDelete(reviewer.id, fetchReviewer) }}>DELETE</button>
+
+                    </div>
+                </div>
+
+
+
+
+
 
             </div>
 
